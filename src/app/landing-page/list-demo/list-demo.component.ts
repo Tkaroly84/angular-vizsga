258093e56ae5f46e815f59ef1ed70a464d729d7b
Tóth Károly <toth.karolycar84@gmail.com>
2024-03-08 06:21:23 +0100

    version 1.7

--- src/app/landing-page/list-demo/list-demo.component.ts
+++ src/app/landing-page/list-demo/list-demo.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { LekerdezesService } from '../../home/lekerdezes.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-list-demo',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './list-demo.component.html',
   styleUrl: './list-demo.component.scss'
 })
 export class ListDemoComponent {
+  a: any[] = [];
+  
+  constructor(
+    private readonly lekerdezesService:LekerdezesService
+  ){}
+
+
+
+  ngOnInit(){
+    this.lekerdezesService.kapottEredmeny$.subscribe(searchResult$ =>{
+      searchResult$.subscribe((data: any) => {
+        this.a.push(data);      });
+    });
+  }
 
 }
